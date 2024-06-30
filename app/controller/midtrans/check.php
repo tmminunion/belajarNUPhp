984bb8737b64f97ffa9279f4fe6927e8d796f67f
tmmin <tmminunion@gmail.com>
2024-07-01 01:59:38 +0700

    new wait

--- app/controller/midtrans/check.php
+++ app/controller/midtrans/check.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Model\Cerit;
+use App\Model\Tabung;
+use App\Model\transaction;
+
 class check
 {
     public function index()
@@ -10,16 +14,105 @@ class check
         if ($status == 'success') {
             switch ($prefix) {
                 case 'KAS':
+                    $datana = transaction::with(['member', 'inputBy'])->where('judul', $data)->first();
+                    if (!$datana) {
+                        to_url('wait?order_id=' . $data);
+                        exit();
+                    }
                     to_url('pembayaran/resume/kas/' . $data);
                 case 'TAB':
+                    $datana = Tabung::with(['member', 'inputBy'])->where('judul', $data)->first();
+                    if (!$datana) {
+                        to_url('wait?order_id=' . $data);
+                        exit();
+                    }
                     to_url('pembayaran/resume/tabungan/' . $data);
                 case 'DON':
+                    $datana = Cerit::with(['member', 'inputBy', 'donasi'])->where('judul', $data)->first();
+                    if (!$datana) {
+                        to_url('wait?order_id=' . $data);
+                        exit();
+                    }
                     to_url('pembayaran/resume/donasi/' . $data);
                 default:
-                    to_url('home/' . $data);
+                    to_url('wait?order_id=' . $data);
             }
         } else {
             to_url('home/' . $data);
+        }
+    }
+
+    public function wait()
+    {
+        $data = $_GET['order_id'];
+        $prefix = substr($data, 0, 3);
+
+        switch ($prefix) {
+            case 'KAS':
+                $datana = transaction::with(['member', 'inputBy'])->where('judul', $data)->first();
+                if (!$datana) {
+                    $response = [
+                        'status' => 'nodata',
+                    ];
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                    return;
+                }
+                $response = [
+                    'status' => 'success',
+                    'link' => get_url('pembayaran/resume/kas/' . $data),
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+                return;
+                break;
+
+            case 'TAB':
+                $datana = Tabung::with(['member', 'inputBy'])->where('judul', $data)->first();
+                if (!$datana) {
+                    $response = [
+                        'status' => 'nodata',
+                    ];
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                    return;
+                }
+                $response = [
+                    'status' => 'success',
+                    'link' =>  get_url('pembayaran/resume/tabungan/' . $data),
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+                return;
+                break;
+
+            case 'DON':
+                $datana = Cerit::with(['member', 'inputBy', 'donasi'])->where('judul', $data)->first();
+                if (!$datana) {
+                    $response = [
+                        'status' => 'nodata',
+                    ];
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                    return;
+                }
+                $response = [
+                    'status' => 'success',
+                    'link' => get_url('pembayaran/resume/donasi/' . $data),
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+                return;
+                break;
+
+            default:
+                $response = [
+                    'status' => 'nodata',
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+                return;
+                break;
         }
     }
 }
